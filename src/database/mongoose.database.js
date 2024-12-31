77299707b3d59b9740bc4e67e26d828301750a14
Bruno Desktop <carecsnay1@gmail.com>
2024-12-31 10:45:50 -0300

    Refactor: change structure.

--- src/database/mongoose.database.js
+++ src/database/mongoose.database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToDatabase = async () => {
     try {
         await mongoose.connect(
-            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ljmw5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ljmw5.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.DB_APP_NAME}`
         );
         return console.log("Conexão sucedida!");
     } catch (error) {
